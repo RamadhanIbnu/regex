be2e134600c9bf145e0904c0764db62a71c7d83b
RamadhanIbnu <ramadhanibnu77@gmail.com>
2020-08-31 22:12:00 +0700

    first commit

--- exercise.js
+++ exercise.js
@@ -1,3 +1,5 @@
+// Soal No.1
+
 // Buat sebuah program untuk mengecek apakah karakter pertama dalam sebuah string mengandung huruf
 // besar atau tidak.
 
@@ -13,25 +15,47 @@ function Besar(a) {
     console.log("Huruf pertama adalah huruf kecil");
   }
 }
-Besar("bcd");
+Besar("kmzway87aa");
 
 
-// SOAL No.2
+// Soal No.2
+
 // Buat sebuah program untuk memvalidasi format nomor handphone
-//     Nomor handphone minimum o10 digit dan maksimum 12 digit
+
+//     Nomor handphone minimum 10 digit dan maksimum 12 digit
+
 //     Hanya menerima digit karakter
 
 
-function vadidasi(a) {
+function validasi(a) {
   regexp = /^\d{10,12}$/;
   if (regexp.test(a)) {
-    console.log("yes benar");
+    console.log("format yang anda masukan benar");
   } else {
-    console.log("Harus number minimal 10 maksimal 12 digit");
+    console.log("Maaf format yang anda masukan salah, minimum 10 digit dan maksimum 12 digit!");
   }
 }
 
 validasi(123244923847);
 
 
-// SOAL 3 :
+// Soal No.3
+
+// Buat sebuah program yang menyediakan kalimat berikut sehingga user mendapatkan pencarian yang persis sama.
+
+//     Bagaimana cara memulai usaha bisnis
+
+//     Bootcamp impact byte
+
+//     Status covid hari ini
+
+function searchWord(a) {
+    regexp = /^Bagaimana cara memulai usaha bisnis$|^Bootcamp Impact Byte$|^Status covid hari ini$/;
+    if(regexp.test(a)) {
+        console.log("kata yang anda cari berhasil ditemukan");
+    } else {
+        console.log("maaf kata yang anda cari tidak dapat ditemukan");
+    }
+}
+
+searchWord("Bootcamp Impact Byte");
